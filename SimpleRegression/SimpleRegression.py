4d4628fb1b59c267f296dc0aaec13762ec3540a4
Piotr Falkowski <falcon777@wp.pl>
2019-05-28 23:08:58 +0200

    Change to statsmodels

--- SimpleRegression/SimpleRegression.py
+++ SimpleRegression/SimpleRegression.py
@@ -6,29 +6,21 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn import datasets, linear_model
 from scipy import stats
+import statsmodels.api as sm
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 if __name__ == '__main__':
     df = pd.read_csv('Eeg classifiers accuracy vs architecture relationship.csv')
     x = df.drop('Accuracy', axis=1)
     y = df[['Accuracy']]
-    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.3,
-                                                  random_state=1)
-    reg = LinearRegression()
+
+    X2 = sm.add_constant(x)
+    est = sm.OLS(y, X2)
+    est2 = est.fit()
     
-    # multiple regression
-    result = reg.fit(X_train[['Iterations','Hidden_nodes']], y_train)
-    y_predicted = reg.predict(X_train[['Iterations','Hidden_nodes']])
-
-    # print results
-    print("Mean squared error: %.2f" % mean_squared_error(y_train, y_predicted))
-    print('R²: %.2f' % r2_score(y_train, y_predicted))
-    #print(result.summary())
-
-    # plot
-    fig, ax = plt.subplots()
-    ax.scatter(y_train, y_predicted)
-    ax.plot([y_train.min(), y_train.max()], [y_train.min(), y_train.max()], 'k--', lw=2)
-    ax.set_xlabel('measured')
-    ax.set_ylabel('predicted')
+    print(est2.summary())
+    sns.regplot(x='Hidden_nodes', y='Accuracy', data=df)
+    plt.show()
+    sns.regplot(x='Iterations', y='Accuracy', data=df)
     plt.show()
